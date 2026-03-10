67bac02782f975bcba3b94614c8492f13ca7addb
laurindolucas <caiolucax2024@gmail.com>
2026-03-10 19:58:59 -0300

    adicionando exercicios da aula de herança

---
+++ unicap-poo/src/aula01_heranca/PessoaJuridica.java
@@ -0,0 +1,33 @@
+package aula01_heranca;
+
+public class PessoaJuridica extends Pessoa{
+    private String cnpj;
+
+    public PessoaJuridica(String nome, int idade, String cnpj) {
+        super(nome, idade);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome(){
+        return super.getNome();
+    }
+    public void setNome(String nome){
+        super.setNome(nome);
+    }
+
+    public int getIdade(){
+        return super.getIdade();
+    }
+
+    public void setIdade(int idade){
+        super.setIdade(idade);
+    }
+}
